62e66e21a171a4c27e98c22e055c5e57b55f8e86
JesseNovice <jesseroylees@gmail.com>
2025-03-22 08:13:34 +1300

    first commit

---
+++ coindox/src/app/page.tsx
@@ -0,0 +1,15 @@
+import HomeOne from "@/component/homes/home-one";
+import Wrapper from "@/layouts/Wrapper";
+
+export const metadata = {
+  title: "Coindox - ICO & Crypto Landing React Next js Template",
+};
+const index = () => {
+  return (
+    <Wrapper>
+      <HomeOne />
+    </Wrapper>
+  )
+}
+
+export default index
